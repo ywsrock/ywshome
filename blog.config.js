74ef1e9252e18f281ef5915690375f4b35a929d8
ywsrock <ywsrock@gmail.com>
2023-06-08 20:54:59 +0900

    listed the postpage

--- blog.config.js
+++ blog.config.js
@@ -4,7 +4,7 @@ const BLOG = {
   email: 'ywsrock@gmail.com',
   link: 'https://www.ywsrock.com',
   newsletter: 'Forever Eighteen',
-  description: 'My home page',
+  description: '自由だけが、生きる意味だ',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Tokyo', // See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
